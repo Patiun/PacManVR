556e09a7764b282f86431edc85d486f212183866
Remlik <Remlikmg@comcast.net>
2018-06-09 18:22:49 -0400

    Ghots can be enabled

--- PacManVR/Assets/Ghost.cs
+++ PacManVR/Assets/Ghost.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class Ghost : MonoBehaviour {
 
+	public float speed;
+	public int playerID;
+
+	private Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown("joystick "+playerID+" button 9")) {
+			Debug.Log ("Boo");
+		}
+		rb.velocity = new Vector3 (Input.GetAxis ("GhostHorizontal_"+playerID), 0, Input.GetAxis ("GhostVertical_"+playerID)).normalized * speed;
 	}
 }
---
+++ PacManVR/Assets/GhostController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostController : MonoBehaviour {
+
+	public Ghost[] ghosts;
+	public float maxGhosts;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 1; i <= maxGhosts; i++) {
+			if (Input.GetKeyDown("joystick "+i+" button 9")) {
+				Debug.Log ("Boo " + i);
+				if (!ghosts [i-1].gameObject.activeInHierarchy) {
+					ghosts [i-1].gameObject.SetActive (true);
+				}
+			}
+		}
+	}
+}
